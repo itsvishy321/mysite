8ab1b4d4dd477ca61bd2b6e8980ab01238b2b3d4
itsvishy321 <102803510+itsvishy321@users.noreply.github.com>
2024-07-24 19:56:05 +0530

    updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#21z2_!tk@ueh7%*au4eqb)mz)iyfikh-_-*-8c*z&2@j6q6&'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG" , cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [config("ALLOWED_HOSTS").split(",")]
 
 
 TINYMCE_DEFAULT_CONFIG = {
@@ -101,7 +102,10 @@ DATABASES = {
         'HOST': 'localhost', 
     }
 }
-
+# DATABASES["default"] = dj_database_url.parse("postgresql://mysite_sr0c_user:eHxUBSUBuWXXEaHSqokCmoYx0lNfSxy8@dpg-cqgg7ddds78s73cen3pg-a.oregon-postgres.render.com/mysite_sr0c")
+DATABASES['default'] = dj_database_url.parse(
+    os.environ.get('DATABASE_URL', config("DATABASE_URL"))
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls'))
 ]
+
+# urlpatterns += staticfiles_urlpatterns
